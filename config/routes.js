e0c2f3a9df09deedc1a4e67b598da4e297d371f0
Jasmine Guzman-Escobar <jasmineguzmanescobar@yahoo.com>
2016-04-05 17:22:49 -0700

    moved to angular

--- config/routes.js
+++ config/routes.js
@@ -6,10 +6,10 @@ var pagesController = require('../controllers/pages');
 // var usersController = require('../controllers/users');
 
 // root path:
-router.get('/', pagesController.welcome);
+// router.get('/', pagesController.welcome);
 
 // navigation resource path:
-router.get('/surf', pagesController.surf);
+// router.get('/surf', pagesController.surf);
 
 // // users resource paths:
 // router.get('/users',     usersController.index);
