55794fcefbba06224f7b34d28646085bea5937c6
Jasmine Guzman-Escobar <jasmineguzmanescobar@yahoo.com>
2016-04-07 23:38:38 -0700

    weather only working in ventura

--- public/js/surfApp/spots/spots.controller.js
+++ public/js/surfApp/spots/spots.controller.js
@@ -12,9 +12,9 @@
 
     vm.slides = [
       {id: 0, title: 'Santa Barbara',  caption: 'Rincon',  image:'http://www.travelgrom.com/wp-content/uploads/Rincon.jpg'},
-      {id: 1, title: 'Ventura',   caption: 'C Street', image: 'http://blog.guytakayamasurfboards.com/wp-content/uploads/2014/09/C-Street-Ventura-County.jpg'},
+      {id: 1, title: 'Ventura', caption: 'C Street', image: 'http://blog.guytakayamasurfboards.com/wp-content/uploads/2014/09/C-Street-Ventura-County.jpg'},
       {id: 2, title: 'Los Angeles', caption: 'Malibu', image: "http://stwww.surfermag.com/files/2013/06/100503_BURKARD_2082.jpg"},
-      {id: 3, title: 'Orange County',  caption: 'Huntington Beach', image: 'http://en.blog.hotelnights.com/wp-content/plugins/php-image-cache/image.php?path=/wp-content/uploads/2013/08/surf-in-california.jpg'}
+      {id: 3, title: 'Orange County', caption: 'Huntington Beach', image: 'http://en.blog.hotelnights.com/wp-content/plugins/php-image-cache/image.php?path=/wp-content/uploads/2013/08/surf-in-california.jpg'}
     ];
 
 
@@ -61,7 +61,7 @@
             });
         }
       }
-      function getCntySptsDeats() {
+       function getCntySptsDeats() {
         if (vm.spotDeats !== '') {
           console.log("vmspotsDeats", vm.spotDeats)
           $http
